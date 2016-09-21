41fb5121ed37887c3756beca1294538c4e772a90
iamzyt <zyt@haha.com>
2016-09-21 22:43:41 +0800

    some modify by z

--- func.c
+++ func.c
@@ -1,3 +1,4 @@
+/* sum of two integers */
 int sum(int x, int y)
 {
 	return x+y;
--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include "func.h"
 
 int main()
 {
 	printf("hello world\n");
+	int a = 0;
+	printf("the sum is: %d\n", sum(5+6));
 
 	return 0;
 }
